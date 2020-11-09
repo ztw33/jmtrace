7458c3a24735a1e763bd4f91841fb1feaf98dee4
ztw33 <tingweizhu33@foxmail.com>
2020-11-09 22:56:29 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# jmtrace
+A tool for tracing shared memory access in a Java program packaged in a jar file.
---
+++ src/test/java/com/tiva/ISER/AppTest.java
@@ -0,0 +1,20 @@
+package com.tiva.ISER;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
